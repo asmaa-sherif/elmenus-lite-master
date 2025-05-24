4667c8856347665eb2880e1954c5f397deda0e91
amelarbab9 <amelarbab997@gmail.com>
2025-05-24 19:00:20 +0400

    CartController and CartServiceImpl

--- src/main/java/spring/practice/elmenus_lite/enums/SuccessAndErrorMessage.java
+++ src/main/java/spring/practice/elmenus_lite/enums/SuccessAndErrorMessage.java
@@ -12,16 +12,21 @@ public enum SuccessAndErrorMessage {
     QUANTITY_UPDATED_SUCCESSFULLY("Quantity updated successfully"),
     CART_ITEM_ADDED_SUCCESSFULLY("Cart item added successfully"),
     CART_ITEM_DELETED_SUCCESSFULLY("Cart item deleted successfully"),
+    CART_CLEARED_SUCCESSFULLY("Cart cleared successfully"),
+    CART_RETRIEVED_SUCCESSFULLY("Cart retrieved successfully"),
 
     // Error Messages
     ERROR("Error"),
     INVALID_INPUT("Invalid input"),
     INVALID_QUANTITY("Invalid quantity"),
     INVALID_CART_ITEM_ID("Invalid cart item ID"),
+    INVALID_CART_ID("Invalid cart ID"),
+    INVALID_CUSTOMER_ID("Invalid customer ID"),
     CAN_NOT_UPDATE_QUANTITY("can not update quantity"),
     CART_ITEM_NOT_FOUND("Cart Item not found with id "),
     CUSTOMER_NOT_FOUND("Customer not found with id "),
     Menu_ITEM_NOT_FOUND("Menu Item not found with id "),
+    CART_NOT_FOUND("Cart not found with id "),
     INVALID_REQUEST("Invalid request data"),
     ;
 
