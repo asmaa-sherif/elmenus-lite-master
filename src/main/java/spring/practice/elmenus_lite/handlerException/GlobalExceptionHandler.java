0214a59539da3fafb7abcb891900bbf967ad5b40
amelarbab9 <amelarbab997@gmail.com>
2025-05-26 14:23:28 +0400

    Handles @Validated validation on @PathVariable and @RequestParam

--- src/main/java/spring/practice/elmenus_lite/handlerException/GlobalExceptionHandler.java
+++ src/main/java/spring/practice/elmenus_lite/handlerException/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package spring.practice.elmenus_lite.handlerException;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,10 +10,19 @@ import spring.practice.elmenus_lite.dto.BaseResponse;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Handles @Valid validation on @RequestBody
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<BaseResponse<Object>> handleValidation(MethodArgumentNotValidException ex) {
-        String message = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+    public ResponseEntity<BaseResponse<Object>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
         return ResponseEntity.badRequest().body(new BaseResponse<>(false, message, null));
     }
+
+    // Handles @Validated validation on @PathVariable or @RequestParam
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<BaseResponse<Object>> handleConstraintViolation(ConstraintViolationException ex) {
+        String message = ex.getConstraintViolations().iterator().next().getMessage();
+        return ResponseEntity.badRequest().body(new BaseResponse<>(false, message, null));
+    }
+
 }
 
