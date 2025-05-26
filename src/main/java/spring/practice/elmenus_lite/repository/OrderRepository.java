65e7f9d6002b48822f21949abafd32399d52eeb3
amelarbab9 <amelarbab997@gmail.com>
2025-05-26 13:53:34 +0400

    Remove unused method from OrderRepository

--- src/main/java/spring/practice/elmenus_lite/repository/OrderRepository.java
+++ src/main/java/spring/practice/elmenus_lite/repository/OrderRepository.java
@@ -1,13 +1,8 @@
 package spring.practice.elmenus_lite.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import spring.practice.elmenus_lite.entity.CartItem;
 import spring.practice.elmenus_lite.entity.Order;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    List<Order> findByRestaurantRestaurantId(Long restaurantId);
 }
