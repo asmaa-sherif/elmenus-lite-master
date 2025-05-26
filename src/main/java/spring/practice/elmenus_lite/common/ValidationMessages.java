3bdcf74ee18bffa98e5ada69f1c9fccbf43716e6
amelarbab9 <amelarbab997@gmail.com>
2025-05-26 14:24:36 +0400

    Use Spring boot validation in CartController

--- src/main/java/spring/practice/elmenus_lite/common/ValidationMessages.java
+++ src/main/java/spring/practice/elmenus_lite/common/ValidationMessages.java
@@ -2,6 +2,7 @@ package spring.practice.elmenus_lite.common;
 
 
 public class ValidationMessages {
+    public static final String INVALID_CART_ID = "Invalid Cart ID";
     public static final String INVALID_CART_ITEM_ID = "Invalid Cart Item ID";
     public static final String INVALID_CUSTOMER_ID = "Invalid Customer ID";
     public static final String INVALID_MENU_ITEM_ID = "Invalid Menu Item ID";
--- src/main/java/spring/practice/elmenus_lite/controller/cart/CartController.java
+++ src/main/java/spring/practice/elmenus_lite/controller/cart/CartController.java
@@ -2,19 +2,25 @@ package spring.practice.elmenus_lite.controller.cart;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import spring.practice.elmenus_lite.dto.BaseResponse;
 import spring.practice.elmenus_lite.dto.cart.CartDto;
 import spring.practice.elmenus_lite.service.cart.CartService;
 
+import static spring.practice.elmenus_lite.common.ValidationMessages.INVALID_CART_ID;
+import static spring.practice.elmenus_lite.common.ValidationMessages.INVALID_CUSTOMER_ID;
+import static spring.practice.elmenus_lite.enums.SuccessAndErrorMessage.CART_CLEARED_SUCCESSFULLY;
+import static spring.practice.elmenus_lite.enums.SuccessAndErrorMessage.CART_RETRIEVED_SUCCESSFULLY;
 
-import static spring.practice.elmenus_lite.enums.SuccessAndErrorMessage.*;
 
 @RestController
 @RequestMapping("/api/v1/cart")
+@Validated
 @Tag(name = "Cart Management", description = "Endpoints for managing customer carts, including retrieval and deletion operations")
 public class CartController {
     private final CartService cartService;
@@ -24,26 +30,20 @@ public class CartController {
         this.cartService = cartService;
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<BaseResponse<CartDto>> deleteCart(@PathVariable Long id) {
-        if (id <= 0) {
-            return ResponseEntity.badRequest().body(new BaseResponse<>(false, INVALID_CART_ID.getMessage(), null));
-        }
+    @DeleteMapping("/{cartId}")
+    public ResponseEntity<BaseResponse<CartDto>> deleteCart(@PathVariable @Min(value = 1, message = INVALID_CART_ID) Long cartId) {
         try {
-            cartService.deleteCart(id);
+            cartService.deleteCart(cartId);
             return ResponseEntity.ok(new BaseResponse<>(true, CART_CLEARED_SUCCESSFULLY.getMessage(), null));
         } catch (EntityNotFoundException ex) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new BaseResponse<>(false, ex.getMessage(), null));
-        } catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new BaseResponse<>(false, e.getMessage(), null));
         }
     }
 
     @GetMapping("/customer/{customerId}")
-    public ResponseEntity<BaseResponse<CartDto>> getCartByCustomerId(@PathVariable Long customerId) {
-        if (customerId <= 0) {
-            return ResponseEntity.badRequest().body(new BaseResponse<>(false, INVALID_CUSTOMER_ID.getMessage(), null));
-        }
+    public ResponseEntity<BaseResponse<CartDto>> getCartByCustomerId(@PathVariable @Min(value = 1, message = INVALID_CUSTOMER_ID) Long customerId) {
         try {
             CartDto cart = cartService.getCartByCustomerId(customerId);
             return ResponseEntity.ok().body(new BaseResponse<>(true, CART_RETRIEVED_SUCCESSFULLY.getMessage(), cart));
